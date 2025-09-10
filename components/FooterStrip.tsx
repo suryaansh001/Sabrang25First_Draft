f995857c03d9c7dd04747d90778b671f9c866fcf
Aman Pratap Singh <amanpratapsingh@jklu.edu.in>
2025-09-10 14:28:41 +0530

    re

--- components/FooterStrip.tsx
+++ components/FooterStrip.tsx
@@ -4,11 +4,20 @@ export default function FooterStrip() {
 	return (
 		<div className="w-full bg-black/20 backdrop-blur-sm border-t border-white/10 py-3 px-4">
 			<div className="max-w-7xl mx-auto flex flex-col items-center justify-center gap-1 text-center">
-				<div className="flex items-center gap-2 text-white/80 text-sm">
-					<a href="https://jklu.edu.in" target="_blank" rel="noopener noreferrer" className="hover:text-white underline-offset-4 hover:underline">jklu.edu.in</a>
-				</div>
+				{/* University site link */}
+				<img src="/JKLU_logo_white.png" alt="JKLU" className="w-15 h-15 object-contain" />
+				{/* Copyright */}
+				<a href="https://jklu.edu.in" target="_blank" rel="noopener noreferrer" className="hover:text-white underline-offset-4 hover:underline">
 				<div className="text-white/70 text-xs sm:text-sm">
 					<span>Sabrang - JK Lakshmipat University © 2025 JKLU. All Right Reserved</span>
+				</div>
+				</a>
+
+				{/* Credits */}
+				<div className="text-white/60 text-xs sm:text-sm">
+					<span>Built and designed by </span>
+					<Link href="/designed-by" className="underline-offset-4 hover:underline hover:text-white">tech_geek</Link>
+				
 				</div>
 			</div>
 		</div>
